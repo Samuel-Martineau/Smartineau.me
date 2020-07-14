89bc59741c5e84ea4995df7ab9560ffed7f8e151
Samuel-Martineau <samumartineau@gmail.com>
2020-07-13 20:10:30 -0400

    Correction du port dans src/server.js

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ import compression from 'compression';
 import express from 'express';
 import sirv from 'sirv';
 
-const { PORT, NODE_ENV } = process.env;
+const { NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 express()
   .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
-  .listen(PORT, (err) => {
+  .listen(process.env.PORT, (err) => {
     if (err) console.log('error', err);
   });
