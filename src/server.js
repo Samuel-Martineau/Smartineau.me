dcc53bfc023ff07f6c4e3136b741dc307ebedaa1
Samuel-Martineau <samumartineau@gmail.com>
2020-07-13 20:16:52 -0400

    Correction du port

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import 'colors';
+
 import * as sapper from '@sapper/server';
 
 import compression from 'compression';
@@ -7,8 +9,12 @@ import sirv from 'sirv';
 const { NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const port = process.env.PORT;
+
 express()
   .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
-  .listen(process.env.PORT, (err) => {
+  .listen(port, (err) => {
+    const stars = '******************'.yellow;
+    console.log(stars + ` Server running on port ${port} ! `.blue.bold + stars);
     if (err) console.log('error', err);
   });
